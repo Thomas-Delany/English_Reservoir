dc004843c8d6fd3e0b2c7cbc0a2141128eee8da1
Thomas <info@englishreservoir.com>
2024-06-08 21:46:12 +0200

    icons added to sidebar

--- eduport_v1.4.1/template/includes/sidebar.php
+++ eduport_v1.4.1/template/includes/sidebar.php
@@ -3,116 +3,32 @@
   <ul class="nav nav-pills flex-column mb-auto">
 
     <!-- Title -->
-    <li class="nav-item lead fw-bold my-2">Reservoir!</li>
-    <li class="nav-item"><a href="#" class="nav-link"><i class="bi bi-house fa-fw me-2"></i>English Grammar</a></li>
-    <li class="nav-item"><a href="#" class="nav-link"><i class="bi bi-house fa-fw me-2"></i>English Verbs</a></li>
-    <li class="nav-item"><a href="#" class="nav-link"><i class="bi bi-house fa-fw me-2"></i>English Confusions</a></li>
-    <li class="nav-item"><a href="#" class="nav-link"><i class="bi bi-house fa-fw me-2"></i>Punctuation</a></li>
-    <li class="nav-item"><a href="#" class="nav-link"><i class="bi bi-house fa-fw me-2"></i>Pronunciation</a></li>
-    <li class="nav-item"><a href="#" class="nav-link"><i class="bi bi-house fa-fw me-2"></i>Blog</a></li>
-    <li class="nav-item"><a href="#" class="nav-link"><i class="bi bi-house fa-fw me-2"></i>Learn for Free!</a></li>
+    <li class="nav-item lead fw-bold my-2">Reservoir!</li> 
+    <li class="nav-item"><a href="https://www.englishreservoir.com/essential-english-grammar/" class="nav-link"><i class="bi bi bi-gear fa-fw me-2"></i>English Grammar</a></li>
+    <li class="nav-item"><a href="https://www.englishreservoir.com/verbs-in-english/" class="nav-link"><i class="bi bi-chat-dots fa-fw me-2"></i>English Verbs</a></li>
+    <li class="nav-item"><a href="https://www.englishreservoir.com/english-confusions/" class="nav-link"><i class="bi bi-question-square fa-fw me-2"></i>English Confusions</a></li>
+    <li class="nav-item"><a href="https://www.englishreservoir.com/punctuation/" class="nav-link"><i class="bi bi-brush fa-fw me-2"></i>Punctuation</a></li>
+    <li class="nav-item"><a href="https://www.englishreservoir.com/pronunciation/" class="nav-link"><i class="bi bi-volume-up fa-fw me-2"></i>Pronunciation</a></li>
+    <li class="nav-item"><a href="https://www.englishreservoir.com/blog/" class="nav-link"><i class="bi bi-book fa-fw me-2"></i>Blog</a></li>
+    <li class="nav-item"><a href="https://www.youtube.com/@EnglishReservoir" class="nav-link"><i class="bi bi-youtube fa-fw me-2"></i>Learn for Free!</a></li>
 
     <!-- Title -->
     <li class="nav-item lead fw-bold my-2">Academy</li>
-    <li class="nav-item"><a href="#" class="nav-link"><i class="bi bi-house fa-fw me-2"></i>B2 First</a></li>
-    <li class="nav-item"><a href="#" class="nav-link"><i class="bi bi-house fa-fw me-2"></i>C1 Advanced</a></li>
-    <li class="nav-item"><a href="#" class="nav-link"><i class="bi bi-house fa-fw me-2"></i>C2 Proficiency</a></li>
-    <li class="nav-item"><a href="#" class="nav-link"><i class="bi bi-house fa-fw me-2"></i>Gramática Esencial</a></li>
-    <li class="nav-item"><a href="#" class="nav-link"><i class="bi bi-house fa-fw me-2"></i>Pronunciación en inglés</a></li>
-    <li class="nav-item"><a href="#" class="nav-link"><i class="bi bi-house fa-fw me-2"></i>Verbos en Inglés</a></li>
+    <li class="nav-item"><a href="https://cursos.englishreservoir.com/p/b2-cambridge-first" class="nav-link"><i class="bi bi-dash fa-fw me-2"></i>B2 First</a></li>
+    <li class="nav-item"><a href="https://cursos.englishreservoir.com/p/c1-cambridge-advanced" class="nav-link"><i class="bi bi-dash fa-fw me-2"></i>C1 Advanced</a></li>
+    <li class="nav-item"><a href="https://cursos.englishreservoir.com/p/c2-cambridge-proficiency" class="nav-link"><i class="bi bi-dash fa-fw me-2"></i>C2 Proficiency</a></li>
+    <li class="nav-item"><a href="https://cursos.englishreservoir.com/p/gramatica-esencial-metodo-divertido" class="nav-link"><i class="bi bi-dash fa-fw me-2"></i>Gramática Esencial</a></li>
+    <li class="nav-item"><a href="https://cursos.englishreservoir.com/p/pronunciacion-en-ingles" class="nav-link"><i class="bi bi-dash fa-fw me-2"></i>Pronunciación en inglés</a></li>
+    <li class="nav-item"><a href="https://cursos.englishreservoir.com/p/verbos-en-ingles-metodo-divertido" class="nav-link"><i class="bi bi-dash fa-fw me-2"></i>Verbos en Inglés</a></li>
 
     <!-- Title -->
     <li class="nav-item lead fw-bold my-2">More about us</li>
-    <li class="nav-item"><a href="#" class="nav-link"><i class="bi bi-house fa-fw me-2"></i>About Us</a></li>
-    <li class="nav-item"><a href="#" class="nav-link"><i class="bi bi-house fa-fw me-2"></i>Our Method</a></li>
-    <li class="nav-item"><a href="#" class="nav-link"><i class="bi bi-house fa-fw me-2"></i>Affiliate Programme</a></li>
-    <li class="nav-item"><a href="#" class="nav-link"><i class="bi bi-house fa-fw me-2"></i>YouTube</a></li>
-    <li class="nav-item"><a href="#" class="nav-link"><i class="bi bi-house fa-fw me-2"></i>Facebook</a></li>
-    <li class="nav-item"><a href="#" class="nav-link"><i class="bi bi-house fa-fw me-2"></i>Twitter/X</a></li>
-    <li class="nav-item"><a href="#" class="nav-link"><i class="bi bi-house fa-fw me-2"></i>Instagram</a></li>
-
-
-
-
-        <!-- menu item 2 -->
-        <li class="nav-item">
-            <a class="nav-link" data-bs-toggle="collapse" href="#collapsepage" role="button"
-            aria-expanded="true" aria-controls="collapsepage">
-            <i class="bi bi-basket fa-fw me-2"></i>Courses
-        </a>
-        <!-- Submenu -->
-        <ul class="nav collapse flex-column show" id="collapsepage"
-								data-bs-parent="#navbar-sidebar">
-								<li class="nav-item"> <a class="nav-link" href="admin-course-list.html">All Courses</a>
-								</li>
-								
-								
-							</ul>
-						</li>
-
-						<!-- Menu item 3 -->
-						<li class="nav-item"> <a class="nav-link" href="admin-student-list.html"><i
-									class="fas fa-user-graduate fa-fw me-2"></i>Students</a></li>
-
-						<!-- Menu item 4 -->
-						<li class="nav-item">
-							<a class="nav-link" data-bs-toggle="collapse" href="#collapseinstructors" role="button"
-								aria-expanded="false" aria-controls="collapseinstructors">
-								<i class="fas fa-user-tie fa-fw me-2"></i>Instructors
-							</a>
-							<!-- Submenu -->
-							<ul class="nav collapse flex-column" id="collapseinstructors"
-								data-bs-parent="#navbar-sidebar">
-								<li class="nav-item"> <a class="nav-link"
-										href="admin-instructor-list.html">Instructors</a></li>
-								<li class="nav-item"> <a class="nav-link" href="admin-instructor-detail.html">Instructor
-										Detail</a></li>
-								<li class="nav-item">
-									<a class="nav-link" href="admin-instructor-request.html">Instructor requests
-										<span class="badge text-bg-success rounded-circle ms-2">2</span>
-									</a>
-								</li>
-							</ul>
-						</li>
-
-						<!-- Menu item 5 -->
-						<li class="nav-item"> <a class="nav-link" href="admin-review.html"><i
-									class="far fa-comment-dots fa-fw me-2"></i>Reviews</a></li>
-
-						<!-- Menu item 6 -->
-						<li class="nav-item"> <a class="nav-link" href="admin-earning.html"><i
-									class="far fa-chart-bar fa-fw me-2"></i>Earnings</a></li>
-
-						<!-- Menu item 7 -->
-						<li class="nav-item"> <a class="nav-link" href="admin-setting.html"><i
-									class="fas fa-user-cog fa-fw me-2"></i>Admin Settings</a></li>
-
-						<!-- Menu item 8 -->
-						<li class="nav-item">
-							<a class="nav-link" data-bs-toggle="collapse" href="#collapseauthentication" role="button"
-								aria-expanded="false" aria-controls="collapseauthentication">
-								<i class="bi bi-lock fa-fw me-2"></i>Authentication
-							</a>
-							<!-- Submenu -->
-							<ul class="nav collapse flex-column" id="collapseauthentication"
-								data-bs-parent="#navbar-sidebar">
-								<li class="nav-item"> <a class="nav-link" href="sign-up.html">Sign Up</a></li>
-								<li class="nav-item"> <a class="nav-link" href="sign-in.html">Sign In</a></li>
-								<li class="nav-item"> <a class="nav-link" href="forgot-password.html">Forgot
-										Password</a></li>
-								<li class="nav-item"> <a class="nav-link" href="admin-error-404.html">Error 404</a></li>
-							</ul>
-						</li>
-
-						<!-- Title -->
-						<li class="nav-item ms-2 my-2">Documentation</li>
-
-						<!-- Menu item 9 -->
-						<li class="nav-item"> <a class="nav-link" href="docs/index.html"><i
-									class="far fa-clipboard fa-fw me-2"></i>Documentation</a></li>
-
-						<!-- Menu item 10 -->
-						<li class="nav-item"> <a class="nav-link" href="docs/changelog.html"><i
-									class="fas fa-sitemap fa-fw me-2"></i>Changelog</a></li>
-                    </ul>
+    <li class="nav-item"><a href="https://www.englishreservoir.com/about-us/" class="nav-link"><i class="bi bi-house fa-fw me-2"></i>About Us</a></li>
+    <li class="nav-item"><a href="https://www.englishreservoir.com/english-reservoir-method/" class="nav-link"><i class="bi bi-easel2 fa-fw me-2"></i>Our Method</a></li>
+    <li class="nav-item"><a href="https://www.englishreservoir.com/affiliate-program-english-reservoir/" class="nav-link"><i class="far fa-chart-bar fa-fw me-2"></i>Affiliate Programme</a></li>
+    <li class="nav-item"><a href="https://www.youtube.com/@EnglishReservoir/featured" class="nav-link"><i class="bi bi-youtube fa-fw me-2"></i>YouTube</a></li>
+    <li class="nav-item"><a href="https://www.facebook.com/Englishreservoir/" class="nav-link"><i class="bi bi-facebook fa-fw me-2"></i>Facebook</a></li>
+    <li class="nav-item"><a href="https://twitter.com/EN_Reservoir" class="nav-link"><i class="bi bi-twitter fa-fw me-2"></i>Twitter/X</a></li>
+    <li class="nav-item"><a href="https://www.instagram.com/english_reservoir" class="nav-link"><i class="bi bi-instagram fa-fw me-2"></i>Instagram</a></li>
+    </ul>
 </div>
