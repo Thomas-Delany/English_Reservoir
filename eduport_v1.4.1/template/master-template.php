24e3d6e33e6a11c9afdff451e64cdcc5ccc4784a
Thomas <info@englishreservoir.com>
2024-06-16 20:50:31 +0200

    adjustments made to config.php

--- eduport_v1.4.1/template/master-template.php
+++ eduport_v1.4.1/template/master-template.php
@@ -1,3 +1,4 @@
+<?php include 'config.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,13 +7,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Dark mode -->
-    <script src="../script.js"></script>
+    <script src="<?php echo BASE_URL; ?>script.js"></script>
   
     <!-- Font Awesome -->
     <script src="https://kit.fontawesome.com/fed40285ab.js" crossorigin="anonymous"></script>
 
     <!-- Favicon -->
-    <link rel="shortcut icon" href="../assets/images/favicon.png">
+    <link rel="shortcut icon" href="<?php echo BASE_URL; ?>assets/images/favicon.png">
 
     <!-- Google Font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -21,14 +22,14 @@
           href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;700&family=Roboto:wght@400;500;700&display=swap">
 
   <!-- Plugins CSS -->
-<link rel="stylesheet" type="text/css" href="../assets/vendor/font-awesome/css/all.min.css">
+<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>assets/vendor/font-awesome/css/all.min.css">
 
 <!-- <link rel="stylesheet" type="text/css" href="assets/vendor/bootstrap-icons/bootstrap-icons.css">-->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-<link rel="stylesheet" type="text/css" href="../assets/vendor/tiny-slider/tiny-slider.css">
+<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>assets/vendor/tiny-slider/tiny-slider.css">
 
     <!-- Theme CSS -->
-    <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>assets/css/style.css">
 </head>
 <body>
   <!-- MAIN CONTENT START -->
@@ -56,12 +57,12 @@
     <div class="back-top"><i class="bi bi-arrow-up-short position-absolute top-50 start-50 translate-middle"></i></div>
 
     <!-- Bootstrap JS -->
-    <script src="../assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 
     <!-- Vendors -->
-    <script src="../assets/vendor/tiny-slider/tiny-slider.js"></script>
+    <script src="<?php echo BASE_URL; ?>assets/vendor/tiny-slider/tiny-slider.js"></script>
 
     <!-- Template Functions -->
-    <script src="../assets/js/functions.js"></script>
+    <script src="<?php echo BASE_URL; ?>assets/js/functions.js"></script>
 </body>
 </html>
