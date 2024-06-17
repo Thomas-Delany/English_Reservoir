b28ed37e2d4aab0704735f569fa10794e4126af3
Thomas <info@englishreservoir.com>
2024-06-17 17:53:15 +0200

    last pages added

---
+++ eduport_v1.4.1/template/blog/index.php
@@ -0,0 +1,9 @@
+<!-- Blog.php -->
+<?php
+$page_title = "Blog";
+$page_heading = "Blog";
+// Page specific content with HTML and Bootstrap elements
+$page_content = "This is some dummy text.";
+// Include the master template
+include '../master-template.php';
+?>
