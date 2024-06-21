85facc990ac8f7339f264d8c12bc0ac61ca5a7db
Thomas <info@englishreservoir.com>
2024-06-21 16:51:04 +0200

    New TOC inserted in all index pages

--- eduport_v1.4.1/template/english-verbs/index.php
+++ eduport_v1.4.1/template/english-verbs/index.php
@@ -1,9 +1,57 @@
 <!-- Verbs in English.php -->
 <?php
-$page_title = "Verbs in English";
-$page_heading = "Verbs in English";
+$page_title = "English Verbs";
+$page_heading = "English Verbs";
 // Page specific content with HTML and Bootstrap elements
-$page_content = "This is some dummy text.";
+$page_content = <<<HTML
+<div class="container mt-4">
+    <div class="row">
+        <!-- Table of Contents -->
+        <nav id="toc" class="col-md-12 rounded-3 p-4 mb-5 toc bg-primary bg-gradient bg-opacity-15">
+            <h2>Table of Contents</h2>
+            <nav class="nav flex-column">
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <div class="d-flex align-items-center">
+                            <i class="bi bi-arrow-right text-primary me-2"></i>
+                            <a class="nav-link fs-5 fw-semibold text-primary" href="#section1">Section 1</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <div class="d-flex align-items-center">
+                            <i class="bi bi-arrow-right text-primary me-2"></i>
+                            <a class="nav-link fs-5 fw-semibold text-primary" href="#section2">Section 2</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <div class="d-flex align-items-center">
+                            <i class="bi bi-arrow-right text-primary me-2"></i>
+                            <a class="nav-link fs-5 fw-semibold text-primary" href="#section3">Section 3</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <div class="d-flex align-items-center">
+                            <i class="bi bi-arrow-right text-primary me-2"></i>
+                            <a class="nav-link fs-5 fw-semibold text-primary" href="#section4">Section 4</a>
+                        </div>
+                    </li>
+                </ul>
+            </nav>
+        </nav>
+        <!-- Content -->
+        <div class="col-md-12">
+            <h3 id="section1">Section 1</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet repudiandae aliquam maxime aperiam sint facere animi facilis quibusdam illo ad.</p>
+            <h3 id="section2" class="mt-5">Section 2</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit odit tempore cum inventore accusamus minima ipsam in praesentium possimus autem!</p>
+            <h3 id="section3" class="mt-5">Section 3</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, reiciendis? Ab dolore provident repellat, est sequi deserunt unde ex voluptatum?</p>
+            <h3 id="section4" class="mt-5">Section 4</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, reiciendis? Ab dolore provident repellat, est sequi deserunt unde ex voluptatum?</p>
+        </div>
+    </div>
+</div>
+HTML;
 // Include the master template
 include '../master-template.php';
 ?>
