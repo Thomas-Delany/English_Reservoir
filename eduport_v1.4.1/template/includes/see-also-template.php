1e86738b7b7bdc6cec18c6919fcb9cecac384ea1
Thomas <info@englishreservoir.com>
2024-07-01 18:04:03 +0200

    initialisation of see also template file

---
+++ eduport_v1.4.1/template/includes/see-also-template.php
@@ -0,0 +1,14 @@
+<!-- includes/see-also-template.php -->
+<?php
+// Define the "See Also" links for this page
+// Ensure $seeAlsoLinks is defined before including this file
+if (!isset($seeAlsoLinks)) {
+  $seeAlsoLinks = '';
+}
+?>
+    <div class="col-12 my-4">
+        <h3>xxxxx</h3>
+        <ul class="list-group">
+            <li class="list-group-item"><a href="<?php echo BASE_URL; ?>#">xxxxx</a></li>
+        </ul>
+    </div>
