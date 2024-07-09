98ff64b747408bad314fafa18d1852cbaf20555b
Thomas <info@englishreservoir.com>
2024-07-09 21:29:00 +0200

    sidebar.php updated with new icons

--- eduport_v1.4.1/template/includes/sidebar.php
+++ eduport_v1.4.1/template/includes/sidebar.php
@@ -4,31 +4,54 @@
 
     <!-- Title -->
     <li class="nav-item lead fw-bold my-2">Reservoir!</li> 
-    <li class="nav-item"><a href="<?php echo BASE_URL; ?>../english-grammar/" class="nav-link"><i class="bi bi bi-gear fa-fw me-2"></i>English Grammar</a></li>
-    <li class="nav-item"><a href="<?php echo BASE_URL; ?>../english-verbs/" class="nav-link"><i class="bi bi-chat-dots fa-fw me-2"></i>English Verbs</a></li>
-    <li class="nav-item"><a href="<?php echo BASE_URL; ?>../english-confusions/" class="nav-link"><i class="bi bi-question-square fa-fw me-2"></i>English Confusions</a></li>
-    <li class="nav-item"><a href="<?php echo BASE_URL; ?>../english-punctuation/" class="nav-link"><i class="bi bi-brush fa-fw me-2"></i>Punctuation</a></li>
-    <li class="nav-item"><a href="<?php echo BASE_URL; ?>../english-pronunciation/" class="nav-link"><i class="bi bi-volume-up fa-fw me-2"></i>Pronunciation</a></li>
-    <li class="nav-item"><a href="<?php echo BASE_URL; ?>../blog/" class="nav-link"><i class="bi bi-book fa-fw me-2"></i>Blog</a></li>
-    <li class="nav-item"><a href="https://www.youtube.com/@EnglishReservoir" class="nav-link"><i class="bi bi-youtube fa-fw me-2"></i>Learn for Free!</a></li>
+
+    <li class="nav-item"><a href="<?php echo BASE_URL; ?>../english-grammar/" class="nav-link"><i class="fa-solid fa-wrench me-3"></i>English Grammar</a></li>
+
+    <li class="nav-item"><a href="<?php echo BASE_URL; ?>../cambridge/" class="nav-link"><i class="fa-solid fa-graduation-cap me-2"></i>Cambridge</a></li>
+
+    <li class="nav-item"><a href="<?php echo BASE_URL; ?>../english-verbs/" class="nav-link"><i class="fa-solid fa-hammer me-2"></i>English Verbs</a></li>
+
+    <li class="nav-item"><a href="<?php echo BASE_URL; ?>../english-confusions/" class="nav-link"><i class="fa-regular fa-face-grin-beam-sweat me-2"></i>English Confusions</a></li>
+
+    <li class="nav-item"><a href="<?php echo BASE_URL; ?>../english-punctuation/" class="nav-link"><i class="fa-solid fa-quote-left me-2"></i>Punctuation</a></li>
+
+    <li class="nav-item"><a href="<?php echo BASE_URL; ?>../english-pronunciation/" class="nav-link"><i class="bi bi-volume-up me-2"></i>Pronunciation</a></li>
+
+    <li class="nav-item"><a href="<?php echo BASE_URL; ?>../blog/" class="nav-link"><i class="bi bi-book me-2"></i>Blog</a></li>
+
+    <li class="nav-item"><a href="https://www.youtube.com/@EnglishReservoir" class="nav-link"><i class="bi bi-youtube me-2"></i>Learn for Free!</a></li>
 
     <!-- Title -->
     <li class="nav-item lead fw-bold my-2">Academy</li>
-    <li class="nav-item"><a href="https://cursos.englishreservoir.com/p/b2-cambridge-first" class="nav-link"><i class="bi bi-dash fa-fw me-2"></i>B2 First</a></li>
-    <li class="nav-item"><a href="https://cursos.englishreservoir.com/p/c1-cambridge-advanced" class="nav-link"><i class="bi bi-dash fa-fw me-2"></i>C1 Advanced</a></li>
-    <li class="nav-item"><a href="https://cursos.englishreservoir.com/p/c2-cambridge-proficiency" class="nav-link"><i class="bi bi-dash fa-fw me-2"></i>C2 Proficiency</a></li>
-    <li class="nav-item"><a href="https://cursos.englishreservoir.com/p/gramatica-esencial-metodo-divertido" class="nav-link"><i class="bi bi-dash fa-fw me-2"></i>Gramática Esencial</a></li>
-    <li class="nav-item"><a href="https://cursos.englishreservoir.com/p/pronunciacion-en-ingles" class="nav-link"><i class="bi bi-dash fa-fw me-2"></i>Pronunciación en inglés</a></li>
-    <li class="nav-item"><a href="https://cursos.englishreservoir.com/p/verbos-en-ingles-metodo-divertido" class="nav-link"><i class="bi bi-dash fa-fw me-2"></i>Verbos en Inglés</a></li>
+
+    <li class="nav-item"><a href="https://cursos.englishreservoir.com/p/b2-cambridge-first" class="nav-link"><i class="bi bi-dash me-2"></i>B2 First</a></li>
+
+    <li class="nav-item"><a href="https://cursos.englishreservoir.com/p/c1-cambridge-advanced" class="nav-link"><i class="bi bi-dash me-2"></i>C1 Advanced</a></li>
+
+    <li class="nav-item"><a href="https://cursos.englishreservoir.com/p/c2-cambridge-proficiency" class="nav-link"><i class="bi bi-dash me-2"></i>C2 Proficiency</a></li>
+
+    <li class="nav-item"><a href="https://cursos.englishreservoir.com/p/gramatica-esencial-metodo-divertido" class="nav-link"><i class="bi bi-dash me-2"></i>Gramática Esencial</a></li>
+
+    <li class="nav-item"><a href="https://cursos.englishreservoir.com/p/pronunciacion-en-ingles" class="nav-link"><i class="bi bi-dash me-2"></i>Pronunciación en inglés</a></li>
+
+    <li class="nav-item"><a href="https://cursos.englishreservoir.com/p/verbos-en-ingles-metodo-divertido" class="nav-link"><i class="bi bi-dash me-2"></i>Verbos en Inglés</a></li>
 
     <!-- Title -->
     <li class="nav-item lead fw-bold my-2">More about us</li>
-    <li class="nav-item"><a href="<?php echo BASE_URL; ?>../about-english-reservoir.php" class="nav-link"><i class="bi bi-house fa-fw me-2"></i>About Us</a></li>
-    <li class="nav-item"><a href="<?php echo BASE_URL; ?>../method-english-reservoir.php" class="nav-link"><i class="bi bi-easel2 fa-fw me-2"></i>Our Method</a></li>
-    <li class="nav-item"><a href="<?php echo BASE_URL; ?>../affiliate.php" class="nav-link"><i class="far fa-chart-bar fa-fw me-2"></i>Affiliate Programme</a></li>
-    <li class="nav-item"><a href="https://www.youtube.com/@EnglishReservoir/featured" target="_blank" rel="noopener noreferrer" class="nav-link"><i class="bi bi-youtube fa-fw me-2"></i>YouTube</a></li>
-    <li class="nav-item"><a href="https://www.facebook.com/Englishreservoir/" target="_blank" rel="noopener noreferrer" class="nav-link"><i class="bi bi-facebook fa-fw me-2"></i>Facebook</a></li>
-    <li class="nav-item"><a href="https://twitter.com/EN_Reservoir" target="_blank" rel="noopener noreferrer" class="nav-link"><i class="bi bi-twitter fa-fw me-2"></i>Twitter/X</a></li>
-    <li class="nav-item"><a href="https://www.instagram.com/english_reservoir" target="_blank" rel="noopener noreferrer" class="nav-link"><i class="bi bi-instagram fa-fw me-2"></i>Instagram</a></li>
+
+    <li class="nav-item"><a href="<?php echo BASE_URL; ?>../about-english-reservoir.php" class="nav-link"><i class="bi bi-house me-2"></i>About Us</a></li>
+
+    <li class="nav-item"><a href="<?php echo BASE_URL; ?>../method-english-reservoir.php" class="nav-link"><i class="bi bi-easel2 me-2"></i>Our Method</a></li>
+
+    <li class="nav-item"><a href="<?php echo BASE_URL; ?>../affiliate.php" class="nav-link"><i class="far fa-chart-bar me-2"></i>Affiliate Programme</a></li>
+
+    <li class="nav-item"><a href="https://www.youtube.com/@EnglishReservoir/featured" target="_blank" rel="noopener noreferrer" class="nav-link"><i class="bi bi-youtube me-2"></i>YouTube</a></li>
+    
+    <li class="nav-item"><a href="https://www.facebook.com/Englishreservoir/" target="_blank" rel="noopener noreferrer" class="nav-link"><i class="bi bi-facebook me-2"></i>Facebook</a></li>
+
+    <li class="nav-item"><a href="https://twitter.com/EN_Reservoir" target="_blank" rel="noopener noreferrer" class="nav-link"><i class="bi bi-twitter me-2"></i>Twitter/X</a></li>
+
+    <li class="nav-item"><a href="https://www.instagram.com/english_reservoir" target="_blank" rel="noopener noreferrer" class="nav-link"><i class="bi bi-instagram me-2"></i>Instagram</a></li>
+
     </ul>
 </div>
