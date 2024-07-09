20c3774f8637365d3635bfe7f32dc0137eafcdb4
Thomas <info@englishreservoir.com>
2024-07-09 20:28:16 +0200

    table-of-contents.php added to all parent pages

--- eduport_v1.4.1/template/english-confusions/index.php
+++ eduport_v1.4.1/template/english-confusions/index.php
@@ -4,53 +4,24 @@ $page_title = "English Confusions";
 $page_heading = "English Confusions";
 $image_url = "../assets/images/english-confusions.png"; // Set the image URL
 $image_alt = "english confusions"; // Set the image alt text
+$show_toc = true; // Set to true to show the Table of Contents
+
 // Page specific content with HTML and Bootstrap elements
 $page_content = <<<HTML
-        <!-- Table of Contents -->
-        <nav id="toc" class="col-md-12 rounded-3 p-4 mb-5 toc bg-primary bg-gradient bg-opacity-15">
-            <h2>Table of Contents</h2>
-            <nav class="nav flex-column">
-                <ul class="nav flex-column">
-                    <li class="nav-item">
-                        <div class="d-flex align-items-center">
-                            <i class="bi bi-arrow-right text-primary me-2"></i>
-                            <a class="nav-link fs-5 fw-semibold text-primary" href="#section1">Section 1</a>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <div class="d-flex align-items-center">
-                            <i class="bi bi-arrow-right text-primary me-2"></i>
-                            <a class="nav-link fs-5 fw-semibold text-primary" href="#section2">Section 2</a>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <div class="d-flex align-items-center">
-                            <i class="bi bi-arrow-right text-primary me-2"></i>
-                            <a class="nav-link fs-5 fw-semibold text-primary" href="#section3">Section 3</a>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <div class="d-flex align-items-center">
-                            <i class="bi bi-arrow-right text-primary me-2"></i>
-                            <a class="nav-link fs-5 fw-semibold text-primary" href="#section4">Section 4</a>
-                        </div>
-                    </li>
-                </ul>
-            </nav>
-        </nav>
-        <!-- Content -->
-        <div class="col-md-12">
-            <h3 id="section1">Section 1</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet repudiandae aliquam maxime aperiam sint facere animi facilis quibusdam illo ad.</p>
-            <h3 id="section2" class="mt-5">Section 2</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit odit tempore cum inventore accusamus minima ipsam in praesentium possimus autem!</p>
-            <!--<div class="ratio ratio-16x9 my-5"></div>-->
-            <h3 id="section3" class="mt-5">Section 3</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, reiciendis? Ab dolore provident repellat, est sequi deserunt unde ex voluptatum?</p>
-            <h3 id="section4" class="mt-5">Section 4</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, reiciendis? Ab dolore provident repellat, est sequi deserunt unde ex voluptatum?</p>
-        </div>
+<p class="lead my-2">This is some additional content that is specific to this child page. It might include introductory text, images, or other elements that don't fit into the structured sections.</p>
 HTML;
+
+// Sections
+$sections = [
+    ['id' => 'section1', 'title' => 'Section 1', 'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet repudiandae aliquam maxime aperiam sint facere animi facilis quibusdam illo ad.'],
+
+    ['id' => 'section2', 'title' => 'Section 2', 'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit odit tempore cum inventore accusamus minima ipsam in praesentium possimus autem!'],
+
+    ['id' => 'section3', 'title' => 'Section 3', 'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, reiciendis? Ab dolore provident repellat, est sequi deserunt unde ex voluptatum?'],
+
+    ['id' => 'section4', 'title' => 'Section 4', 'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, reiciendis? Ab dolore provident repellat, est sequi deserunt unde ex voluptatum?']
+
+];
 // Include the master template
 include '../master-template.php';
 ?>
