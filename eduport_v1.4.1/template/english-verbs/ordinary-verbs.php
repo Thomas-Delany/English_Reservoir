f1e2e0571b19ef81909a948cd4d9b2437dbf6d2a
Thomas <info@englishreservoir.com>
2024-07-10 18:49:50 +0200

    remaining child pages now being served with php variables

--- eduport_v1.4.1/template/english-verbs/ordinary-verbs.php
+++ eduport_v1.4.1/template/english-verbs/ordinary-verbs.php
@@ -2,53 +2,48 @@
 <?php
 $page_title = "Ordinary Verbs";
 $page_heading = "Ordinary Verbs";
+// Set the image URL if it exists - $image_url = "../link to image url"; 
+// Set the image alt text if it exists - $image_alt = "image alt";
+
 // Page specific content with HTML and Bootstrap elements
 $page_content = <<<HTML
-        <!-- Table of Contents -->
-        <nav id="toc" class="col-md-12 rounded-3 p-4 my-5 toc bg-primary bg-gradient bg-opacity-15">
-            <h2>Table of Contents</h2>
-            <nav class="nav flex-column">
-                <ul class="nav flex-column">
-                    <li class="nav-item">
-                        <div class="d-flex align-items-center">
-                            <i class="bi bi-arrow-right text-primary me-2"></i>
-                            <a class="nav-link fs-5 fw-semibold text-primary" href="#section1">Section 1</a>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <div class="d-flex align-items-center">
-                            <i class="bi bi-arrow-right text-primary me-2"></i>
-                            <a class="nav-link fs-5 fw-semibold text-primary" href="#section2">Section 2</a>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <div class="d-flex align-items-center">
-                            <i class="bi bi-arrow-right text-primary me-2"></i>
-                            <a class="nav-link fs-5 fw-semibold text-primary" href="#section3">Section 3</a>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <div class="d-flex align-items-center">
-                            <i class="bi bi-arrow-right text-primary me-2"></i>
-                            <a class="nav-link fs-5 fw-semibold text-primary" href="#section4">Section 4</a>
-                        </div>
-                    </li>
-                </ul>
-            </nav>
-        </nav>
-        <!-- Content -->
-        <div class="col-md-12">
-            <h3 id="section1">Section 1</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet repudiandae aliquam maxime aperiam sint facere animi facilis quibusdam illo ad.</p>
-            <h3 id="section2" class="mt-5">Section 2</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit odit tempore cum inventore accusamus minima ipsam in praesentium possimus autem!</p>
-            <!--<div class="ratio ratio-16x9 my-5"></div>-->
-            <h3 id="section3" class="mt-5">Section 3</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, reiciendis? Ab dolore provident repellat, est sequi deserunt unde ex voluptatum?</p>
-            <h3 id="section4" class="mt-5">Section 4</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, reiciendis? Ab dolore provident repellat, est sequi deserunt unde ex voluptatum?</p>
-        </div>
+<p>This is some additional content that is specific to this child page. It might include introductory text, images, or other elements that don't fit into the structured sections.</p>
 HTML;
+
+// Define sections
+$sections = [
+    [
+        'id' => 'section1',
+        'title' => 'Section 1',
+        'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet repudiandae aliquam maxime aperiam sint facere animi facilis quibusdam illo ad.'
+    ],
+    [
+        'id' => 'section2',
+        'title' => 'Section 2',
+        'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit odit tempore cum inventore accusamus minima ipsam in praesentium possimus autem!',
+        // 'image' => true
+    ],
+    [
+        'id' => 'section3',
+        'title' => 'Section 3',
+        'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, reiciendis? Ab dolore provident repellat, est sequi deserunt unde ex voluptatum?'
+    ],
+    [
+        'id' => 'section4',
+        'title' => 'Section 4',
+        'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, reiciendis? Ab dolore provident repellat, est sequi deserunt unde ex voluptatum?',
+        // YouTube link 'youtube' => '<iframe></iframe>'
+    ],
+];
+// Define the "see also" links
+$see_also_links = [
+    ['url' => 'page1.php', 'title' => 'Page 1'],
+    ['url' => 'page2.php', 'title' => 'Page 2'],
+    ['url' => 'page3.php', 'title' => 'Page 3'],
+    ['url' => 'page4.php', 'title' => 'Page 4'],
+    ['url' => 'page5.php', 'title' => 'Page 5']
+];
+
 // Include the master template
 include '../master-template.php';
 ?>
