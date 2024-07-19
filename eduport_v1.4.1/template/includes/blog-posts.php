9ac7c5c060c5974aa78cd8fde3ea69ab67268427
Thomas <info@englishreservoir.com>
2024-07-19 19:32:09 +0200

    urls corrected in array

--- eduport_v1.4.1/template/includes/blog-posts.php
+++ eduport_v1.4.1/template/includes/blog-posts.php
@@ -5,7 +5,7 @@ $blog_posts = [
         'date' => 'January 1, 2024',
         'author' => 'English Reservoir',
         'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat iusto consectetur hic earum nostrum maiores soluta! Consectetur beatae totam officia praesentium, cumque alias nihil at voluptatem ut ipsa iure nesciunt?',
-        'link' => 'blog-post1.php',
+        'link' => '/blog/blog-post1.php',
         'image' => '../assets/images/courses/b2_first.png',
         'image_alt' => 'Blog Image 1'
     ],
@@ -14,7 +14,7 @@ $blog_posts = [
         'date' => 'February 1, 2024',
         'author' => 'English Reservoir',
         'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat iusto consectetur hic earum nostrum maiores soluta! Consectetur beatae totam officia praesentium, cumque alias nihil at voluptatem ut ipsa iure nesciunt?',
-        'link' => 'blog-post2.php',
+        'link' => '/blog/blog-post2.php',
         'image' => '../assets/images/courses/b2_first.png',
         'image_alt' => 'Blog Image 2'
     ],
