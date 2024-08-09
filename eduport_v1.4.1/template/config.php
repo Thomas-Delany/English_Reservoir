55c5a82516226f6fa926af34ae6306c341913d25
ilikelinux <info@englishreservoir.com>
2024-08-09 17:36:28 +0200

    htaccess edited

--- eduport_v1.4.1/template/config.php
+++ eduport_v1.4.1/template/config.php
@@ -1,4 +1,4 @@
 <?php
-define('BASE_URL', '/');
+define('BASE_URL', 'https://englishreservoir.co.uk/');
 ?>
 
--- eduport_v1.4.1/template/master-template.php
+++ eduport_v1.4.1/template/master-template.php
@@ -1,4 +1,3 @@
-<?php include __DIR__ . '/config.php'; ?>
 <!DOCTYPE html>
 <html lang="en"> 
 <head>
