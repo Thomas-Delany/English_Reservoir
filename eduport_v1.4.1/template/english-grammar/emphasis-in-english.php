2be3768a832746540938c8daf5f4baf3557016e7
ilikelinux <info@englishreservoir.com>
2024-08-28 17:49:16 +0200

    inserted new pages with child template

---
+++ eduport_v1.4.1/template/english-grammar/emphasis-in-english.php
@@ -0,0 +1,108 @@
+<!-- Child Page Template.php -->
+<?php
+$page_title = "Emphasis in English";
+$page_heading = "Emphasis in English";
+// Set the image URL if it exists - $image_url = BASE_URL . "link to image url"; 
+// Set the image alt text if it exists - $image_alt = "image alt";
+$show_toc = false; // Set to true to show a Table of Contents
+
+// Page specific content with HTML and Bootstrap elements
+$page_content = <<<HTML
+<p>This is some additional content that is specific to this child page. It might include introductory text, images, or other elements that don't fit into the structured sections.</p>
+HTML; 
+
+
+// Define sections
+$sections = [
+    [
+        'id' => 'section1',
+        'title' => 'Section 1',
+        'content' => [
+            'Lorem ipsum dolor amet, consectetur adipisicing elit. Velit odit tempore cum inventore accusamus minima ipsam in praesentium possimus autem!',
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit odit tempore cum inventore accusamus minima ipsam in praesentium possimus autem!',
+            // Add more paragraphs as needed
+        ],
+        // Add a table if needed
+        /*'table' => [
+            'headers' => ['Column 1', 'Column 2'],
+            'rows' => [
+                ['Lorem ipsum dolor amet', 'Lorem ipsum dolor amet'],
+                ['Lorem ipsum dolor amet', 'Lorem ipsum dolor amet'],
+                ['Lorem ipsum dolor amet', 'Lorem ipsum dolor amet'],
+            ]
+        ],*/ 
+    ],
+    [
+        'id' => 'section2',
+        'title' => 'Section 2',
+        'content' => [
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit odit tempore cum inventore accusamus minima ipsam in praesentium possimus autem!',
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit odit tempore cum inventore accusamus minima ipsam in praesentium possimus autem!',
+            // Add more paragraphs as needed
+        ],
+          // Course images
+          'cursos-de-ingles' => true, 
+          'b2-cambridge-first' => false, 
+          'c1-cambridge-advanced' => false, 
+          'c2-cambridge-proficiency' => false, 
+          'gramatica-esencial' => false, 
+          'verbos-en-ingles' => false,
+          'pronunciacion-en-ingles' => false,
+    ],
+    [
+        'id' => 'section3',
+        'title' => 'Section 3',
+        'content' => [
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit odit tempore cum inventore accusamus minima ipsam in praesentium possimus autem!',
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit odit tempore cum inventore accusamus minima ipsam in praesentium possimus autem!',
+            // Add more paragraphs as needed
+        ],
+        // YouTube clip 'youtube' => '<iframe></iframe>'
+    ],
+    [
+        'id' => 'section4',
+        'title' => 'Section 4',
+        'content' => [
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit odit tempore cum inventore accusamus minima ipsam in praesentium possimus autem!',
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit odit tempore cum inventore accusamus minima ipsam in praesentium possimus autem!',
+            // Add more paragraphs as needed
+        ],
+        // Add bullets if needed
+        /*'bullets' => [
+            [
+                'First bullet point set',
+                'Second bullet point set',
+            ],
+            [
+                'First bullet point of second set',
+                'Second bullet point of second set',
+            ]
+        ],*/
+    ],
+    [
+        'id' => 'section5',
+        'title' => 'Section 5',
+        'content' => [
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit odit tempore cum inventore accusamus minima ipsam in praesentium possimus autem!',
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit odit tempore cum inventore accusamus minima ipsam in praesentium possimus autem!',
+            // Add more paragraphs as needed
+        ],
+    ],
+];
+
+/*
+// Define the "see also" links
+$see_also_links = [
+    ['url' => BASE_URL . 'page1.php', 'title' => 'Page 1'],
+    ['url' => BASE_URL . 'page2.php', 'title' => 'Page 2'],
+    ['url' => BASE_URL . 'page3.php', 'title' => 'Page 3'],
+    ['url' => BASE_URL . 'page4.php', 'title' => 'Page 4'],
+    ['url' => BASE_URL . 'page5.php', 'title' => 'Page 5']
+]; */
+
+
+
+// Include the master template
+include __DIR__ . '/../master-template.php';
+?>
+
